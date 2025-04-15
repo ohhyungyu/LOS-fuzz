6d929385c26c8a06d6664bb2d7c8c3dbfd8bf26c
Hyungyu OH(OJingA) <42890945+ohhyungyu@users.noreply.github.com>
2025-04-15 20:12:17 +0900

    COPYRIGHT UPLOAD
    all copyright ADDED

--- ros2_automatic_fuzzer/ros2_fuzzer/fuzzing_utils/generate_cpp_file.py
+++ ros2_automatic_fuzzer/ros2_fuzzer/fuzzing_utils/generate_cpp_file.py
@@ -1,3 +1,11 @@
+# This file is based on ros2-fuzz (https://github.com/rosin-project/ros2_fuzz)
+# Copyright (c) 2021 Francisco Martínez Lasaca
+#
+# Modified by Sejong university(Republic of Korea) Hyungyu Oh in 2025
+# Changes: 
+#
+# Licensed under the MIT License.
+
 import os
 from zenlog import log as logging
 from pathlib import Path
